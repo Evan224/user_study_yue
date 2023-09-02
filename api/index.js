e48dd2028fe88a15eb33e37ee864640f5c0c0425
Evan <2248392717@qq.com>
2023-09-02 18:50:24 +0200

    test the deploy again

--- api/index.js
+++ api/index.js
@@ -25,6 +25,18 @@ function shuffleArray(array) {
     }
 }
 
+app.get('/api', (req, res) => {
+    const path = `/api/item/${v4()}`;
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+  });
+  
+  app.get('/api/item/:slug', (req, res) => {
+    const { slug } = req.params;
+    res.end(`Item: ${slug}`);
+  });
+
 app.get('/images', async (req, res) => {
     try {
         const params = {
